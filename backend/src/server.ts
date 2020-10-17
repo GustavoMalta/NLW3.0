c58120dff341d9233ab512a487853503e49d3f24
GustavoMalta <Gustavo.a.Malta@gmail.com>
2020-10-17 15:01:32 -0300

    Aula03

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,10 +11,11 @@ const app = express()
 
 app.use(express.json())
 
+app.use(cors())
+
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname,'..','uploads')))
 
 app.use(errorHandler)
-app.use(cors())
 
 app.listen(3334)
